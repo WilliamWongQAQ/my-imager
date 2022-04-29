09f5f964ea49f9718a9e1f5079cdc6f65cfe73dc
wangwenliang <915840547@qq.com>
2022-04-29 17:56:01 +0800

    first commit

--- omniimager/test.sh
+++ omniimager/test.sh
@@ -13,3 +13,5 @@ EOF
 losetup /dev/loop7 -o 1048576 /opt/omni-workspace/test.raw
 mkfs.ext4 /dev/loop7
 mount -t ext4 /dev/loop7 ${2}
+
+grub2-install --boot-directory=/opt/omni-workspace/rootfs/boot/ --target=i386-pc --modules=part_msdos  /opt/omni-workspace/test.raw
